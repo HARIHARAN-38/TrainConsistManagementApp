d5afd647fa6553f6d5d78f7c79ba54e2e199535d
HARIHARAN-38 <dhariharan38@gmail.com>
2026-03-24 11:13:43 +0530

    UC4: Maintain Ordered Bogie IDs

---
+++ src/UseCase4TrainConsistMgmnt.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class UseCase4TrainConsistMgmnt{
+    public static void main(String[] args) {
+        System.out.println("============================\n");
+        System.out.println("Maintain Ordered Bogie Consist");
+        System.out.println("============================\n");
+        List<String> trainConsist = new LinkedList<>();
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
+        trainConsist.add(2, "Pantry Car");
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+        System.out.println(trainConsist);
+    }
+}
